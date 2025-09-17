e31741bd9862f6df1606bed488aac94b2292e374
Natalia <holovshyk@gmail.com>
2025-09-17 19:27:30 +0300

    feat: add company management pages and components
    - Implemented company listing and details pages with respective components.
    - Added a header and sidebar for navigation.
    - Created reusable components for company rows and tables.
    - Integrated search functionality with a search input component.
    - Added SVG icons for UI elements.
    - Established a layout structure for admin pages.
    - Included avatar image for user profile in the header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import StatusLabel, { Status } from '@/app/components/status-label';
+// import StatusLabel, { Status } from '@/app/components/status-label';
 import './globals.css';
 import AddCompanyButton from './components/add-company-button';
 
@@ -6,10 +6,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home Page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       <AddCompanyButton />
     </main>
   );
